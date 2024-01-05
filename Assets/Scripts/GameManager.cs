8983106d4ab1548c02c31847913c6ae86ae0de46
Jake Bayer <jake@maxgaming.net>
2024-01-05 16:15:40 -0500

    Set up player spawning

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Transform _playerSpawnPoint;
+    [SerializeField] private GameObject _playerCamera;
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = FindFirstObjectByType<GameManager>();
+            return _instance;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnPlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void SpawnPlayer()
+    {
+        var spawnPlayer = Instantiate(Resources.Load<GameObject>("PlayerController"), 
+            _playerSpawnPoint.transform.position, 
+            _playerSpawnPoint.transform.rotation);
+
+        var cameraTarget = spawnPlayer.transform.Find("CameraTarget");
+        spawnPlayer.GetComponent<PlayerController>().camera = Camera.main.transform;
+        _playerCamera.GetComponent<CinemachineVirtualCamera>().Follow = cameraTarget;
+        _playerCamera.GetComponent<PlayerCamera>().cameraTarget = cameraTarget;
+    }
+}
