1dd3a7440dce5abbb49bce3c3ff0681cedd451df
Jake Bayer <jakebayerct@gmail.com>
2024-02-28 11:26:25 -0500

    - Set up main menu - Imported TMP - Imported UniTask

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,40 @@
+using Cysharp.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region editor-bound
+    public async void PlayGame()
+    {
+        var loadScene = SceneManager.LoadSceneAsync(1);
+        while(!loadScene.isDone)
+        {
+            await UniTask.Yield();
+        }
+    }
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+    #endregion
+}
